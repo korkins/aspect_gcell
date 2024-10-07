685191f9acb60f268fec0f5a26213bd971e23ef6
korkins <30602383+korkins@users.noreply.github.com>
2024-10-07 09:42:28 -0400

    Add files via upload

---
+++ src/paths.h
@@ -0,0 +1,9 @@
+﻿// Set paths to HITRAN's *.par and TIPS files
+//
+int const
+	path_len_max = 256;
+char const
+	path_hitdb[path_len_max] = "./hitran/",
+	path_TIPS[path_len_max] = "./hitran/TIPS/";
+//
+/*------------------------------------------------------------------------------------------------*/
