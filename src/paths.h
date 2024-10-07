279641cbc4f4064c87af87b233730bf47f8ebff5
korkins <30602383+korkins@users.noreply.github.com>
2024-10-07 09:41:05 -0400

    Delete src directory

--- src/paths.h
+++
@@ -1,9 +0,0 @@
-﻿// Set paths to HITRAN's *.par and TIPS files
-//
-int const
-	path_len_max = 256;
-char const
-	path_hitdb[path_len_max] = "./hitran/",
-	path_TIPS[path_len_max] = "./hitran/TIPS/";
-//
-/*------------------------------------------------------------------------------------------------*/
